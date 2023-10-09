9d3e8c590840b1753588fe03dd7521a5d6ea456e
ma1one <202019060120@stu.cdut.edu.cn>
2023-10-09 15:20:03 +0800

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,17 +6,17 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
-    let mut c = input.chars();
-    match c.next() {
-        None => String::new(),
-        Some(first) => ???,
-    }
+	let mut c = input.chars();
+	match c.next() {
+		None => String::new(),
+		Some(first) => {
+			first.to_ascii_uppercase().to_string() + &input[1..]
+		}
+	}
 }
 
 // Step 2.
@@ -24,7 +24,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+	let mut res = Vec::new();
+	for str in words {
+		res.push(capitalize_first(str))
+	}
+	res
 }
 
 // Step 3.
@@ -32,32 +36,36 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+	let mut res = String::new();
+	for str in words {
+		res += &capitalize_first(str)
+	}
+	res
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_success() {
-        assert_eq!(capitalize_first("hello"), "Hello");
-    }
+	#[test]
+	fn test_success() {
+		assert_eq!(capitalize_first("hello"), "Hello");
+	}
 
-    #[test]
-    fn test_empty() {
-        assert_eq!(capitalize_first(""), "");
-    }
+	#[test]
+	fn test_empty() {
+		assert_eq!(capitalize_first(""), "");
+	}
 
-    #[test]
-    fn test_iterate_string_vec() {
-        let words = vec!["hello", "world"];
-        assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
-    }
+	#[test]
+	fn test_iterate_string_vec() {
+		let words = vec!["hello", "world"];
+		assert_eq!(capitalize_words_vector(&words), ["Hello", "World"]);
+	}
 
-    #[test]
-    fn test_iterate_into_string() {
-        let words = vec!["hello", " ", "world"];
-        assert_eq!(capitalize_words_string(&words), "Hello World");
-    }
+	#[test]
+	fn test_iterate_into_string() {
+		let words = vec!["hello", " ", "world"];
+		assert_eq!(capitalize_words_string(&words), "Hello World");
+	}
 }
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,35 +27,37 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
+extern "C" {
+	#[link_name = "my_demo_function"]
+	fn my_demo_function(a: u32) -> u32;
 
-extern "Rust" {
-    fn my_demo_function(a: u32) -> u32;
-    fn my_demo_function_alias(a: u32) -> u32;
+	#[link_name = "my_demo_function"]
+	fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
-    // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
-        a
-    }
+	// No `extern` equals `extern "Rust"`.
+	#[no_mangle]
+	extern "C" fn my_demo_function(a: u32) -> u32 {
+		a
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_success() {
-        // The externally imported functions are UNSAFE by default
-        // because of untrusted source of other languages. You may
-        // wrap them in safe Rust APIs to ease the burden of callers.
-        //
-        // SAFETY: We know those functions are aliases of a safe
-        // Rust function.
-        unsafe {
-            my_demo_function(123);
-            my_demo_function_alias(456);
-        }
-    }
+	#[test]
+	fn test_success() {
+		// The externally imported functions are UNSAFE by default
+		// because of untrusted source of other languages. You may
+		// wrap them in safe Rust APIs to ease the burden of callers.
+		//
+		// SAFETY: We know those functions are aliases of a safe
+		// Rust function.
+		unsafe {
+			my_demo_function(123);
+			my_demo_function_alias(456);
+		}
+	}
 }
